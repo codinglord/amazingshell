4a57d3514cb0177c1b81dc2684d3b11200dd1894
witthaya thatarath <codeengine@live.com>
2014-02-14 12:25:43 +0700

    - Established create-azurewebsite.

---
+++ azure/CreateWebSite/Create-AzureWebsite.ps1
@@ -0,0 +1,49 @@
+﻿
+
+[xml]$SharedConfig = Get-Content -Path "C:\Projects\GITsDEMO\amazingshell\azure\config.config"
+
+
+
+
+
+
+
+Function Create-WebSite()
+{
+
+    #Shortname config
+    $conf = $SharedConfig 
+
+
+    #Import publishsetting
+    Import-AzurePublishSettingsFile `
+        -PublishSettingsFile $conf.SelectSingleNode("/configuration/create-azurewebsite/publishsettings")."#text"
+
+
+
+
+    New-AzureWebsite `
+        -Hostname $conf.SelectSingleNode("/configuration/create-azurewebsite/hostname")."#text"`
+        -Location $conf.SelectSingleNode("/configuration/create-azurewebsite/location")."#text"
+        
+
+
+
+
+    #Publish-AzureServiceProject -ServiceName 
+
+}
+
+
+
+
+
+
+Function Main(){
+
+
+    Create-WebSite
+
+}
+
+Main 
